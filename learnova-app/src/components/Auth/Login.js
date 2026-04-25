a5f6afc3b791b7f93f54e7d5a3e3ea50a11a663f
mandorianark-create <mandorianark@gmail.com>
2026-04-25 12:33:32 +0100

    Create Login component for authentication

---
+++ learnova-app/src/components/Auth/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            <h2>Login</h2>
+            {/* Implement login form with Supabase auth */}
+        </div>
+    );
+};
+
+export default Login;
